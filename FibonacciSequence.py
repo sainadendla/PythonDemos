4cc2d138f7945e1d3fcf31c9a730251ffe8b891c
AkhilNadendla <anadendla@g.ucla.edu>
2015-06-20 13:24:00 -0700

    finished up some samples

--- FibonacciSequence.py
+++ FibonacciSequence.py
@@ -1,11 +1,14 @@
 print('Fibonacci Sequence')
 a = int(input('Enter a Number: '))
 b = 1
-c = 1
-while b<= a:
-    if b==1:
-        print(b)
-    if c==1:
-        print(c)
-    b+c =d
+c = 0
+e = 2
+print(c)
+print(b)
+while e <= a:
+    d = b+c
+    print(d)
+    c = b
+    b = d
+    e += 1
     
--- PrimeNumberChecker.py
+++ PrimeNumberChecker.py
@@ -1,2 +1,13 @@
+import sys
 print("Prime Number Checker")
 a = int(input('Enter a Number: '))
+if a <= 1:
+    print('Not Prime')
+    sys.exit()
+for i in range(2,a):
+    if (a % i==0):
+        print('Not Prime')
+        break
+else:
+    print('Prime')
+                
